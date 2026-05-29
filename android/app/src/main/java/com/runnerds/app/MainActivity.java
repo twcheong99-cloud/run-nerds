ce3c8d5c604130390f5744adaedebbb2dbee9ba2
twcheong99-cloud <twcheong99@gmail.com>
2026-05-29 16:00:30 +0900

    Polish mobile safe area handling

--- android/app/src/main/java/com/runnerds/app/MainActivity.java
+++ android/app/src/main/java/com/runnerds/app/MainActivity.java
@@ -1,5 +1,35 @@
 package com.runnerds.app;
 
+import android.graphics.Color;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        Window window = getWindow();
+        window.setStatusBarColor(Color.parseColor("#06100a"));
+        window.setNavigationBarColor(Color.parseColor("#050d08"));
+
+        int flags = window.getDecorView().getSystemUiVisibility();
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            flags &= ~View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR;
+        }
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            flags &= ~View.SYSTEM_UI_FLAG_LIGHT_NAVIGATION_BAR;
+            window.setNavigationBarDividerColor(Color.parseColor("#050d08"));
+        }
+        window.getDecorView().setSystemUiVisibility(flags);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            window.setNavigationBarContrastEnforced(false);
+            window.setStatusBarContrastEnforced(false);
+        }
+    }
+}
